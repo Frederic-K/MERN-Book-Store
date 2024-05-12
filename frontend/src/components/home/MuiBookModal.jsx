3c436d7f031fb82747158a6563de137de1ba2d5f
Frederic-K <frederic.kreuder.pro@gmail.com>
2024-05-12 17:12:27 +0200

    modal css

--- frontend/src/components/home/MuiBookModal.jsx
+++ frontend/src/components/home/MuiBookModal.jsx
@@ -1,8 +1,12 @@
 // MUI
 import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
+import { IconButton } from '@mui/material'
 import Typography from '@mui/material/Typography'
 import Modal from '@mui/material/Modal'
+import HighlightOffIcon from '@mui/icons-material/HighlightOff'
+import { red, grey } from '@mui/material/colors'
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 
 const MuiBookModal = ({ book, showModal, setShowModal }) => {
   const handleClose = () => {
@@ -26,14 +30,75 @@ const MuiBookModal = ({ book, showModal, setShowModal }) => {
             bgcolor: 'background.paper',
             border: '2px solid #000',
             boxShadow: 24,
-            p: 4,
+            p: 2,
+            borderRadius: '25px',
           }}
         >
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Typography
+              sx={{
+                color: 'white',
+                bgcolor: red[300],
+                py: '5px',
+                px: '10px',
+                borderRadius: '10px',
+                alignSelf: 'center',
+              }}
+            >
+              {book.publishYear}
+            </Typography>
+            <IconButton onClick={handleClose} aria-label="close">
+              <HighlightOffIcon />
+            </IconButton>
+          </Box>
+
+          <Box sx={{ my: '5px' }}>
+            <Typography sx={{ color: grey[500] }}>{book._id}</Typography>
+          </Box>
+
+          <Box component="section" sx={{ my: '10px' }}>
+            <Box
+              sx={{
+                display: 'flex',
+                gap: '5px',
+                alignItems: 'center',
+                my: '5px',
+              }}
+            >
+              <LibraryBooksIcon sx={{ color: red[300] }} />
+              <Typography variant="h6" sx={{ fontWeight: 500 }}>
+                {book.title}
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                gap: '5px',
+                alignItems: 'center',
+                my: '5px',
+              }}
+            >
+              <AccountCircleIcon sx={{ color: red[300] }} />
+              <Typography variant="h6" sx={{ fontWeight: 500 }}>
+                {book.author}
+              </Typography>
+            </Box>
+          </Box>
+          <Typography
+            id={`book-preview-${book.title}`}
+            // variant="h6"
+            // component="h2"
+            sx={{ fontSize: '20px' }}
+          >
+            Anything You want to show
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          <Typography id="modal-modal-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quia
+            voluptatum sint. Nisi impedit libero eveniet cum vitae qui expedita
+            necessitatibus assumenda laboriosam, facilis iste cumque a pariatur
+            nesciunt cupiditate voluptas? Quis atque earum voluptate dolor nisi
+            dolorum est? Deserunt placeat cumque quo dicta architecto, dolore
+            vitae voluptate sequi repellat!
           </Typography>
         </Box>
       </Modal>
