756e188ae349dcd205540c0e03bdbcf3553524eb
Frederic-K <110983707+Frederic-K@users.noreply.github.com>
2024-11-19 17:57:38 +0100

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555
 
 export const mongoDBURL =
-  'mongodb+srv://root:TdYiZfeeljT7nuab@book-store-mern-stack.lxtz9d1.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN-Stack'
+  'your_key'
